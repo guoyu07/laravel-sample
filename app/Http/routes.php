ec3c92fe96335d063c70203fe77d04fe5ecb76b4
sqrtqiezi <sqrtqiezi@foxmail.com>
2016-12-26 15:36:55 +0800

    Finish login and logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,5 +16,8 @@ get('/help', 'StaticPagesController@help')->name('help');
 get('/about', 'StaticPagesController@about')->name('about');
 
 get('signup', 'UsersController@create')->name('signup');
-
 resource('users', 'UsersController');
+
+get('login', 'SessionsController@create')->name('login');
+post('login', 'SessionsController@store')->name('login');
+delete('logout', 'SessionsController@destroy')->name('logout');
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,24 +5,16 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <header class="navbar navbar-fixed-top navbar-inverse">
-        <div class="container">
-            <div class="col-md-offset-1 col-md-10">
-                <a href="/" id="logo">Sample App</a>
-                <nav>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="{{ route('help') }}">帮助</a></li>
-                        <li><a href="#">登录</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
+    @include('layouts._header')
 
     <div class="container">
-        @include('shared.messages')
-        @yield('content')
-        @include('layouts._footer')
+        <div class="col-md-offset-1 col-md-10">
+            @include('shared.messages')
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </div>
+
+    <script src="/js/app.js"></script>
 </body>
 </html>
